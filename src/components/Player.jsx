b7ea39c86ed368fbc35dabfeaff977df712f851d
Pablo Llanes <pllanesparamo@gmail.com>
2024-12-17 16:56:02 -0500

    Move final countdown into its own repo

---
+++ src/components/Player.jsx
@@ -0,0 +1,20 @@
+import { useState, useRef } from 'react';
+
+export default function Player() {
+    const playerName = useRef();
+    const [enteredPlayer, setEnteredPlayer] = useState('');
+
+    const handleClick = () => {
+        setEnteredPlayer(playerName.current.value);
+        playerName.current.value = '';
+    };
+    return (
+        <section id='player'>
+            <h2>Welcome {enteredPlayer ?? 'unknown entity'}</h2>
+            <p>
+                <input type='text' ref={playerName} />
+                <button onClick={handleClick}>Set Name</button>
+            </p>
+        </section>
+    );
+}
